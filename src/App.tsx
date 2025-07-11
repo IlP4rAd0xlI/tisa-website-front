a5bb53ce199b03ccae237764b7ae2ced288cd389
IlP4rAd0xlI <ilp4rad0xli@gmail.com>
2025-07-11 16:28:43 +0200

    Create : Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Gallery from './pages/Gallery'
+import Cart from './pages/Cart'
 
 function App() {
   return (
     <>
-      <h1>App.jsx</h1>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/cart" element={<Cart />} />
+      </Routes>
     </>
   )
 }
